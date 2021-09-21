75ee383b0904ad848ffbcbdfc235b62519358186
Naveen-Reddy237 <naveeneee237@gmail.com>
2021-09-21 18:07:35 +0530

    Add project files.

---
+++ DemoConversation/Conversation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DynamicsOmnichannelBotPoc.DemoConversation
+{
+    public class Conversation
+    {
+        public string WelcomeMessage { get; set; }
+
+        public Dictionary<string, string> ConversationDictionary { get; set; }
+
+        public Dictionary<string, Dictionary<string, string>> EscalationDictionary { get; set; }
+
+        public Dictionary<string, Dictionary<string, string>> EndConversationDictionary { get; set; }
+    }
+}
