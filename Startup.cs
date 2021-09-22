d04fc3f5ec6b62ea5ba31bb64642f7c4da073b14
Naveen-Reddy237 <61746690+Naveen-Reddy237@users.noreply.github.com>
2021-09-22 20:11:12 +0530

    Startup commit

--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,8 @@ namespace DynamicsOmnichannelBotPoc
 {
     public class Startup
     {
+       private const string BotOpenIdMetadataKey = "BotOpenIdMetadata";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,34 +28,36 @@ namespace DynamicsOmnichannelBotPoc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers().AddNewtonsoftJson();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            // Create the Bot Framework Adapter with error handling enabled.
+            // Create the Bot Framework Adapter.
             services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
+
+            // Create the Dynamics Omnichannel middleware.
+            services.AddSingleton<DynamicsOmnichannelMiddleware>();
 
             // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
             services.AddTransient<IBot, DynamicsOmnichannelAgentBot>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseHsts();
+            }
 
-            app.UseDefaultFiles()
-                .UseStaticFiles()
-                .UseWebSockets()
-                .UseRouting()
-                .UseAuthorization()
-                .UseEndpoints(endpoints =>
-                {
-                    endpoints.MapControllers();
-                });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseWebSockets();
 
-            // app.UseHttpsRedirection();
+            app.UseMvc();
         }
+    }
     }
 }
