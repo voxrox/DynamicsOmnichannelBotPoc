5fa15118ebc5cf47f2bf1a65d056b04a7618804f
Naveen-Reddy237 <61746690+Naveen-Reddy237@users.noreply.github.com>
2021-09-22 20:33:51 +0530

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -42,7 +42,7 @@ namespace DynamicsOmnichannelBotPoc
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
